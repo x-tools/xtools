fdd71e3b0bd1b0efd9d40646b2337a14508af379
Matthew Bowker <matthewrbowker@me.com>
2016-03-30 14:27:27 -0600

    Basic framework

---
+++ includes.php
@@ -0,0 +1,3 @@
+<?php
+require_once("config.inc.php");
+if (file_exists("includes/config.inc.local.php")) {require_once("includes/config.inc.local.php");}
---
+++ includes/config.inc.php
@@ -0,0 +1,3 @@
+<?php
+
+// CONFIGURATION OPTIONS HERE
