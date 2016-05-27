98919b99057875ccf8d8a240e9d67b9f82bde574
Matthew Bowker <matthewrbowker@me.com>
2016-05-26 23:29:36 -0600

    Remove all files

--- includes.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once("config.inc.php");
-if (file_exists("includes/config.inc.local.php")) {require_once("includes/config.inc.local.php");}
--- includes/config.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// CONFIGURATION OPTIONS HERE
