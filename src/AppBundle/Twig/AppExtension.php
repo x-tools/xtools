4c7bbdb5b3eb331621cbfdc4d2694e0e7924bb59
Matthew Bowker <matthewrbowker@me.com>
2016-05-29 12:39:53 -0600

    Create method to generate links in twig templates

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -2,14 +2,23 @@
 
 namespace AppBundle\Twig;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class AppExtension extends \Twig_Extension
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
     public function getFunctions()
     {
         return [
             new \Twig_SimpleFunction('request_time', [$this, 'requestTime'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('memory_usage', [$this, 'requestMemory'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('peak_memory', [$this, 'requestPeakMemory'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('link', [$this, 'generateLink'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -24,6 +33,11 @@ class AppExtension extends \Twig_Extension
 
     public function requestPeakMemory() {
         return 0;
+    }
+
+    public function generateLink($page, $text, $class = "") {
+        $path = $this->container->getParameter('web.path');
+        return "<a href='$path/$page' class='$class'>$text</a>";
     }
 
     public function getName()
