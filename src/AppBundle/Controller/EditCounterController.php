648c1960123078f6da6ea6c3cf69f57760acac0e
Matthew Bowker <matthewrbowker@me.com>
2016-05-29 13:07:03 -0600

    Softlink for public_html

--- src/AppBundle/Controller/EditCounterController.php
+++ src/AppBundle/Controller/EditCounterController.php
@@ -15,9 +15,7 @@ class EditCounterController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('editCounter/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('editCounter/index.html.twig', []);
     }
 
     /**
