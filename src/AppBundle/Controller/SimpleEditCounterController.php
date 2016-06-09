0ebf5d5c1b0fffdabbb40529503083217138ed4d
Matthew Bowker <matthewrbowker@me.com>
2016-06-08 18:45:50 -0600

    Cleanup routes, comments

--- src/AppBundle/Controller/SimpleEditCounterController.php
+++ src/AppBundle/Controller/SimpleEditCounterController.php
@@ -11,41 +11,31 @@ class SimpleEditCounterController extends Controller
 {
     /**
      * @Route("/sc", name="SimpleEditCounter")
+     * @Route("/sc/", name="SimpleEditCounterSlash")
+     * @Route("/sc/index.php", name="SimpleEditCounterIndexPhp")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('simpleEditCounter/index.html.twig', [
-            "pageTitle" => "Quick, Dirty, Simple Edit Counter",
-            "subtitle" => "Quick user contribution analysis",
-            'page' => "sc",
-        ]);
-    }
 
-    /**
-     * @Route("/sc/get", name="SimpleEditCounterRedirect")
-     */
-
-    public function getAction() {
+        // Grab the request object, grab the values out of it.
         $request = Request::createFromGlobals();
 
         $project = $request->query->get('project');
         $username = $request->query->get('user');
 
-        if ($project == "") {
-            // Redirect back to the index
-            return $this->redirectToRoute("SimpleEditCounter", array());
+        if ($project != "" && $username != "") {
+            return $this->redirectToRoute("SimpleEditCounterResult", array('project'=>$project, 'username' => $username));
         }
-        if ($username == "") {
-            // Redirect to the project selection
-            return $this->redirectToRoute("SimpleEditCounterProject", array(
-                'project' => $project
-            ));
+        else if ($project != "") {
+            return $this->redirectToRoute("SimpleEditCounterProject", array('project'=>$project));
         }
 
-        return $this->redirectToRoute('SimpleEditCounterResult', array(
-            'project' => $project,
-            'username' => $username));
+        // Otherwise fall through.
+        return $this->render('simpleEditCounter/index.html.twig', [
+            "pageTitle" => "Quick, Dirty, Simple Edit Counter",
+            "subtitle" => "Quick user contribution analysis",
+            'page' => "sc",
+        ]);
     }
 
     /**
@@ -58,7 +48,6 @@ class SimpleEditCounterController extends Controller
             "pageTitle" => "Quick, Dirty, Simple Edit Counter",
             "subtitle" => "Quick user contribution analysis",
             'project' => $project,
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
 
@@ -66,7 +55,13 @@ class SimpleEditCounterController extends Controller
      * @Route("/sc/{project}/{username}", name="SimpleEditCounterResult")
      */
     public function resultAction($project, $username) {
+
+        $username = ucfirst($username);
+
+        // Grab the connection to the meta database
         $conn = $this->get('doctrine')->getManager("meta")->getConnection();
+
+        // Create the query we're going to run against the meta database
         $wikiQuery = $conn->createQueryBuilder();
         $wikiQuery
             ->select(['name','url'])
@@ -76,18 +71,22 @@ class SimpleEditCounterController extends Controller
             ->setParameter("project", $project);
         $wikiStatement = $wikiQuery->execute();
 
+        // Fetch the wiki data
         $wikis = $wikiStatement->fetchAll();
 
+        // Throw an exception if we can't find the wiki
         if (sizeof($wikis) <1) {
             throw new Exception("Unknown wiki \"$project\"");
         }
 
-        dump($wikis);
-
+        // Grab the data we need out of it.
         $wikiName = $wikis[0]['name'];
         $url = $wikis[0]['url'];
 
+        // Grab the connection to the replica database (which is separate from the above)
         $conn = $this->get('doctrine')->getManager("replicas")->getConnection();
+
+        // Prepare the query and execute
         $resultQuery = $conn->prepare( "
 			SELECT 'id' as source, user_id as value FROM mw_user WHERE user_name = :username
 			UNION
@@ -101,19 +100,21 @@ class SimpleEditCounterController extends Controller
         $resultQuery->bindParam("username", $username);
         $resultQuery->execute();
 
+        // Fetch the result data
         $results = $resultQuery->fetchAll();
 
-        dump($results);
-
-        if (sizeof($results) <1) {
+        // Unknown user
+        if (sizeof($results) < 3) {
             throw new Exception("Unknown user \"$username\"");
         }
 
+        // Initialize the variables - just so we don't get variable undefined errors if there is a problem
         $id = "";
         $arch = "";
         $rev = "";
         $groups = "";
 
+        // Iterate over the results, putting them in the right variables
         foreach($results as $row) {
             if($row["source"] == "id") {
                 $id = $row["value"];
@@ -129,14 +130,17 @@ class SimpleEditCounterController extends Controller
             }
         }
 
+        // Remove the last comma and space
         if (strlen($groups) > 2) {
             $groups = substr($groups, 0, -2);
         }
 
+        // If the user isn't in any groups, show a message.
         if (strlen($groups) == 0) {
             $groups = "No groups";
         }
 
+        // Assign the values and display the template
         return $this->render('simpleEditCounter/result.html.twig', [
             'title' => "Plain and simple edit counter | $username@$wikiName",
             'page' => "sc",
