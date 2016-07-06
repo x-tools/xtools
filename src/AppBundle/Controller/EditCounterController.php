5259a6622c459d6c914929f35740de2b78720c88
Matthew Bowker <matthewrbowker@me.com>
2016-07-05 21:15:19 -0600

    Beginning of data for the edit counter

--- src/AppBundle/Controller/EditCounterController.php
+++ src/AppBundle/Controller/EditCounterController.php
@@ -11,41 +11,32 @@ class EditCounterController extends Controller
 {
     /**
      * @Route("/ec", name="EditCounter")
+     * @Route("/ec/", name="EditCounterSlash")
+     * @Route("/ec/index.php", name="EditCounterIndexPhp")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('editCounter/index.html.twig', [
-            'page' => "ec",
-            'pageTitle' => "tool_ec",
-            'subtitle' => "tool_ec_desc"
-        ]);
-    }
 
-    /**
-     * @Route("/ec/get", name="EditCounterRedirect")
-     */
-
-    public function getAction() {
+        // Grab the request object, grab the values out of it.
         $request = Request::createFromGlobals();
 
         $project = $request->query->get('project');
         $username = $request->query->get('user');
 
-        if ($project == "") {
-            // Redirect back to the index
-            return $this->redirectToRoute("EditCounter", array());
+        if ($project != "" && $username != "") {
+            return $this->redirectToRoute("EditCounterResult", array('project'=>$project, 'username' => $username));
         }
-        if ($username == "") {
-            // Redirect to the project selection
-            return $this->redirectToRoute("EditCounterProject", array(
-                'project' => $project
-            ));
+        else if ($project != "") {
+            return $this->redirectToRoute("EditCounterProject", array('project'=>$project));
         }
 
-        return $this->redirectToRoute('EditCounterResult', array(
-            'project' => $project,
-            'username' => $username));
+        // Otherwise fall through.
+        return $this->render('editCounter/index.html.twig', [
+            "pageTitle" => "tool_ec",
+            "subtitle" => "tool_ec_desc",
+            'page' => "ec",
+            'title' => "tool_ec",
+        ]);
     }
 
     /**
@@ -98,13 +89,81 @@ class EditCounterController extends Controller
             $dbName .= "_p";
         }
 
+        if (substr($dbName, -2) != "_p") {
+            $dbName .= "_p";
+        }
+
         // Grab the connection to the replica database (which is separate from the above)
         $conn = $this->get('doctrine')->getManager("replicas")->getConnection();
 
+        // Prepare the query and execute
+        $resultQuery = $conn->prepare( "
+			SELECT 'id' as source, user_id as value FROM $dbName.user WHERE user_name = :username
+			UNION
+			SELECT 'arch' as source, COUNT(*) AS value FROM $dbName.archive_userindex WHERE ar_user_text = :username
+			UNION
+			SELECT 'rev' as source, COUNT(*) AS value FROM $dbName.revision_userindex WHERE rev_user_text = :username
+			UNION
+			(SELECT 'first_rev' as source, rev_timestamp FROM $dbName.`revision_userindex` WHERE rev_user_text = :username ORDER BY rev_timestamp ASC LIMIT 1)
+			UNION
+			(SELECT 'latest_rev' as source, rev_timestamp FROM $dbName.`revision_userindex` WHERE rev_user_text = :username ORDER BY rev_timestamp DESC LIMIT 1)
+			UNION
+			SELECT 'groups' as source, ug_group as value FROM $dbName.user_groups JOIN user on user_id = ug_user WHERE user_name = :username
+            ");
 
+        $resultQuery->bindParam("username", $username);
+        $resultQuery->execute();
+
+        // Fetch the result data
+        $results = $resultQuery->fetchAll();
+
+        // Unknown user
+        if (sizeof($results) < 3) {
+            throw new Exception("Unknown user \"$username\"");
+        }
+
+        // Initialize the variables - just so we don't get variable undefined errors if there is a problem
+        $id = "";
+        $arch = "";
+        $rev = "";
+        $first_rev = "";
+        $latest_rev = "";
+        $groups = "";
+
+        // Iterate over the results, putting them in the right variables
+        foreach($results as $row) {
+            if($row["source"] == "id") {
+                $id = $row["value"];
+            }
+            if($row["source"] == "arch") {
+                $arch = $row["value"];
+            }
+            if($row["source"] == "rev") {
+                $rev = $row["value"];
+            }
+            if($row["source"] == "first_rev") {
+                $first_rev = $row["value"];
+            }
+            if($row["source"] == "latest_rev") {
+                $latest_rev = $row["value"];
+            }
+            if($row["source"] == "groups") {
+                $groups .= $row["value"]. ", ";
+            }
+        }
+
+        // Remove the last comma and space
+        if (strlen($groups) > 2) {
+            $groups = substr($groups, 0, -2);
+        }
+
+        // If the user isn't in any groups, show a message.
+        if (strlen($groups) == 0) {
+            $groups = "No groups";
+        }
 
         return $this->render('editCounter/result.html.twig', [
-            'title' => "$username@$dbName Edit Counter",
+            'title' => "tool_ec",
             'page' => "ec",
             'project' => $project,
             'username' => $username,
@@ -112,6 +171,13 @@ class EditCounterController extends Controller
             'name' => $wikiName,
             'url' => $url,
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'user_id' => $id,
+            'user_groups' => $groups,
+            'deleted_edits' => $arch,
+            'total_edits' => $rev + $arch,
+            'live_edits' => $rev,
+            'first_rev' => $first_rev,
+            'latest_rev' => $latest_rev,
         ]);
     }
 }
