c71188000c334fd10cd15d0a0aa8209930d98ac2
Matthew Bowker <matthewrbowker@me.com>
2016-11-20 20:46:43 -0700

    Update cache clearing script

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
 sudo php bin/console cache:clear --env=prod
 
-mkdir var/cache/prod/annotations
+if [ ! -d "var/cache/prod/annotations" ]; then
+    mkdir var/cache/prod/annotations
+fi
 
-sudo chmod 777 var/cache/prod/annotations
+if [ ! -d "var/cache/prod/twig" ]; then
+    mkdir var/cache/prod/twig
+fi
+
+sudo chmod -R 777 var/cache/prod/annotations
+
+sudo chmod -R 777 var/cache/prod/twig
