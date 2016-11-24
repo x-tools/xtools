87c877a7e61fe8e5cfc85a41e8d973b852ffc233
Matthew Bowker <matthewrbowker@me.com>
2016-11-23 21:11:05 -0700

    Include small things that have been missing round 2

--- src/AppBundle/Controller/AdminScoreController.php
+++ src/AppBundle/Controller/AdminScoreController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdminScoreController extends Controller
 {
