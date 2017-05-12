91b50240a4ccf4ba5d06436429bc2393d4e7f81b
MusikAnimal <musikanimal@gmail.com>
2017-05-12 18:15:44 -0400

    [WIP] AutoEdits: view nonautomated contributions!

--- web/static/js/autoedits.js
+++ web/static/js/autoedits.js
@@ -1,47 +1,51 @@
-(function () {
-    $(document).ready(function () {
-        var newData;
+$(function() {
+    var newData;
 
-        $('.tools-table').on('click', '.tools-toggle', function () {
-            if (!newData) {
-                // countsByTool must be cloned
-                newData = Object.assign({}, window.countsByTool);
-            }
+    $('.tools-table').on('click', '.tools-toggle', function () {
+        if (!newData) {
+            // countsByTool must be cloned
+            newData = Object.assign({}, window.countsByTool);
+        }
 
-            var index = $(this).data('index'),
-                tool = $(this).data('tool'),
-                $row = $(this).parents('tr');
+        var index = $(this).data('index'),
+            tool = $(this).data('tool'),
+            $row = $(this).parents('tr');
 
-            // must use .attr instead of .prop as sorting script will clone DOM elements
-            if ($(this).attr('data-disabled') === 'true') {
-                newData[tool] = window.countsByTool[tool];
-                window.toolsChart.data.datasets[0].data[index] = parseInt(newData[tool], 10);
-                $(this).attr('data-disabled', 'false');
-            } else {
-                delete newData[tool];
-                window.toolsChart.data.datasets[0].data[index] = null;
-                $(this).attr('data-disabled', 'true');
-            }
+        // must use .attr instead of .prop as sorting script will clone DOM elements
+        if ($(this).attr('data-disabled') === 'true') {
+            newData[tool] = window.countsByTool[tool];
+            window.toolsChart.data.datasets[0].data[index] = parseInt(newData[tool], 10);
+            $(this).attr('data-disabled', 'false');
+        } else {
+            delete newData[tool];
+            window.toolsChart.data.datasets[0].data[index] = null;
+            $(this).attr('data-disabled', 'true');
+        }
 
-            // gray out row in table
-            $(this).parents('tr').toggleClass('excluded');
+        // gray out row in table
+        $(this).parents('tr').toggleClass('excluded');
 
-            // change the hover icon from a 'x' to a '+'
-            $(this).find('.glyphicon').toggleClass('glyphicon-remove').toggleClass('glyphicon-plus');
+        // change the hover icon from a 'x' to a '+'
+        $(this).find('.glyphicon').toggleClass('glyphicon-remove').toggleClass('glyphicon-plus');
 
-            // update stats
-            var total = 0;
-            Object.keys(newData).forEach(function (tool) {
-                total += parseInt(newData[tool], 10);
-            });
-            var toolsCount = Object.keys(newData).length;
-            $('.tools--tools').text(
-                toolsCount.toLocaleString() + " " +
-                $.i18n('num-tools', toolsCount)
-            );
-            $('.tools--count').text(total.toLocaleString());
-
-            window.toolsChart.update();
+        // update stats
+        var total = 0;
+        Object.keys(newData).forEach(function (tool) {
+            total += parseInt(newData[tool], 10);
         });
+        var toolsCount = Object.keys(newData).length;
+        $('.tools--tools').text(
+            toolsCount.toLocaleString() + " " +
+            $.i18n('num-tools', toolsCount)
+        );
+        $('.tools--count').text(total.toLocaleString());
+
+        window.toolsChart.update();
     });
-})();
+
+    if ($('.non-auto-edits-container')[0]) {
+        $.getJSON('/api/nonautomated_edits/en.wikipedia.org/L235/all/0').then(function (data) {
+            $('.non-auto-edits-container').html(data.markup);
+        });
+    }
+});
