90950802dddab81775175549ce332cb6975e4dc3
MusikAnimal <musikanimal@gmail.com>
2017-06-12 14:34:16 -0600

    SimpleEditCounter: use new wiki Twig macros

--- src/AppBundle/Controller/SimpleEditCounterController.php
+++ src/AppBundle/Controller/SimpleEditCounterController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Xtools\ProjectRepository;
+use Xtools\User;
+use Xtools\UserRepository;
 
 class SimpleEditCounterController extends Controller
 {
@@ -21,9 +23,9 @@ class SimpleEditCounterController extends Controller
     public function indexAction($project = null)
     {
 
-        $lh = $this->get("app.labs_helper");
+        $lh = $this->get('app.labs_helper');
 
-        $lh->checkEnabled("sc");
+        $lh->checkEnabled('sc');
 
         // Grab the request object, grab the values out of it.
         $request = Request::createFromGlobals();
@@ -31,11 +33,11 @@ class SimpleEditCounterController extends Controller
         $projectQuery = $request->query->get('project');
         $username = $request->query->get('username', $request->query->get('user'));
 
-        if ($projectQuery != "" && $username != "") {
-            $routeParams = [ 'project'=>$projectQuery, 'username' => $username ];
-            return $this->redirectToRoute("SimpleEditCounterResult", $routeParams);
-        } elseif ($projectQuery != "") {
-            return $this->redirectToRoute("SimpleEditCounterProject", [ 'project'=>$projectQuery ]);
+        if ($projectQuery != '' && $username != '') {
+            $routeParams = [ 'project' => $projectQuery, 'username' => $username ];
+            return $this->redirectToRoute('SimpleEditCounterResult', $routeParams);
+        } elseif ($projectQuery != '') {
+            return $this->redirectToRoute('SimpleEditCounterProject', [ 'project' => $projectQuery ]);
         }
 
         // Otherwise fall through.
@@ -52,29 +54,29 @@ class SimpleEditCounterController extends Controller
      */
     public function resultAction($project, $username)
     {
-        $lh = $this->get("app.labs_helper");
+        $lh = $this->get('app.labs_helper');
+        $lh->checkEnabled('sc');
 
-        $lh->checkEnabled("sc");
+        $user = new User($username);
+        $username = $user->getUsername();
 
-        $username = ucfirst($username);
+        $project = ProjectRepository::getProject($project, $this->container);
 
-        $projectData = ProjectRepository::getProject($project, $this->container);
+        // if (!$project->exists()) {
+        //     $this->addFlash('notice', ['invalid-project', $projectQuery]);
+        //     return $this->redirectToRoute('SimpleEditCounter');
+        // }
 
-        if (!$projectData->exists()) {
-            $this->addFlash("notice", ["invalid-project", $project]);
-            return $this->redirectToRoute("SimpleEditCounter");
-        }
+        $dbName = $project->getDatabaseName();
+        $url = $project->getUrl();
 
-        $dbName = $projectData->getDatabaseName();
-        $url = $projectData->getUrl();
-
-        $userTable = $lh->getTable("user", $dbName);
-        $archiveTable = $lh->getTable("archive", $dbName);
-        $revisionTable = $lh->getTable("revision", $dbName);
-        $userGroupsTable = $lh->getTable("user_groups", $dbName);
+        $userTable = $lh->getTable('user', $dbName);
+        $archiveTable = $lh->getTable('archive', $dbName);
+        $revisionTable = $lh->getTable('revision', $dbName);
+        $userGroupsTable = $lh->getTable('user_groups', $dbName);
 
         // Grab the connection to the replica database (which is separate from the above)
-        $conn = $this->get('doctrine')->getManager("replicas")->getConnection();
+        $conn = $this->get('doctrine')->getManager('replicas')->getConnection();
 
         // Prepare the query and execute
         $resultQuery = $conn->prepare("
@@ -88,45 +90,45 @@ class SimpleEditCounterController extends Controller
 				FROM $userGroupsTable JOIN $userTable on user_id = ug_user WHERE user_name = :username
 			");
 
-        $resultQuery->bindParam("username", $username);
+        $resultQuery->bindParam('username', $username);
         $resultQuery->execute();
 
         if ($resultQuery->errorCode() > 0) {
-            $this->addFlash("notice", [ "no-result", $username ]);
-            return $this->redirectToRoute("SimpleEditCounterProject", [ "project"=>$project ]);
+            $this->addFlash('notice', [ 'no-result', $username ]);
+            return $this->redirectToRoute('SimpleEditCounterProject', [ 'project' => $projectQuery ]);
         }
 
         // Fetch the result data
         $results = $resultQuery->fetchAll();
 
         // Initialize the variables - just so we don't get variable undefined errors if there is a problem
-        $id = "";
-        $arch = "";
-        $rev = "";
-        $groups = "";
+        $id = '';
+        $arch = '';
+        $rev = '';
+        $groups = '';
 
         // Iterate over the results, putting them in the right variables
         foreach ($results as $row) {
-            if ($row["source"] == "id") {
-                $id = $row["value"];
+            if ($row['source'] == 'id') {
+                $id = $row['value'];
             }
-            if ($row["source"] == "arch") {
-                $arch = $row["value"];
+            if ($row['source'] == 'arch') {
+                $arch = $row['value'];
             }
-            if ($row["source"] == "rev") {
-                $rev = $row["value"];
+            if ($row['source'] == 'rev') {
+                $rev = $row['value'];
             }
-            if ($row["source"] == "groups") {
-                $groups .= $row["value"]. ", ";
+            if ($row['source'] == 'groups') {
+                $groups .= $row['value']. ', ';
             }
         }
 
         // Unknown user - If the user is created the $results variable will have 3 entries.
         // This is a workaround to detect non-existent IPs.
         if (count($results) < 3 && $arch == 0 && $rev == 0) {
-            $this->addFlash('notice', [ "no-result", $username ]);
+            $this->addFlash('notice', [ 'no-result', $username ]);
 
-            return $this->redirectToRoute("SimpleEditCounterProject", [ "project"=>$project ]);
+            return $this->redirectToRoute('SimpleEditCounterProject', [ 'project' => $projectQuery ]);
         }
 
         // Remove the last comma and space
@@ -136,14 +138,14 @@ class SimpleEditCounterController extends Controller
 
         // If the user isn't in any groups, show a message.
         if (strlen($groups) == 0) {
-            $groups = "---";
+            $groups = '---';
         }
 
-        $globalGroups = "";
+        $globalGroups = '';
 
-        if (boolval($this->getParameter("app.single_wiki"))) {
+        if (boolval($this->getParameter('app.single_wiki'))) {
             // Retrieving the global groups, using the ApiHelper class
-            $api = $this->get("app.api_helper");
+            $api = $this->get('app.api_helper');
             $globalGroups = $api->globalGroups($url, $username);
         }
 
@@ -151,7 +153,7 @@ class SimpleEditCounterController extends Controller
         return $this->render('simpleEditCounter/result.html.twig', [
             'xtPage' => 'sc',
             'xtTitle' => $username,
-            'username' => $username,
+            'user' => $user,
             'project' => $project,
             'project_url' => $url,
             'id' => $id,
