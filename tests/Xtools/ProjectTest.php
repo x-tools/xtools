f77e1c6eafc539b1fc14e53d1031407db74c5c56
MusikAnimal <musikanimal@gmail.com>
2017-10-04 12:01:08 -0400

    Improve test coverage of Project class

--- tests/Xtools/ProjectTest.php
+++ tests/Xtools/ProjectTest.php
@@ -112,6 +112,37 @@ class ProjectTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Get the relative URL to the index.php script.
+     */
+    public function testGetScript()
+    {
+        $projectRepo = $this->getMock(ProjectRepository::class);
+        $projectRepo->expects($this->once())
+            ->method('getMetadata')
+            ->willReturn([
+                'general' => [
+                    'script' => '/w/index.php'
+                ],
+            ]);
+        $project = new Project('testWiki');
+        $project->setRepository($projectRepo);
+        $this->assertEquals('/w/index.php', $project->getScript());
+
+        // No script from API.
+        $projectRepo2 = $this->getMock(ProjectRepository::class);
+        $projectRepo2->expects($this->once())
+            ->method('getMetadata')
+            ->willReturn([
+                'general' => [
+                    'scriptPath' => '/w'
+                ],
+            ]);
+        $project2 = new Project('testWiki');
+        $project2->setRepository($projectRepo2);
+        $this->assertEquals('/w/index.php', $project2->getScript());
+    }
+
+    /**
      * A user or a whole project can opt in to displaying restricted statistics.
      * @dataProvider optedInProvider
      * @param string[] $optedInProjects List of projects.
@@ -165,6 +196,56 @@ class ProjectTest extends PHPUnit_Framework_TestCase
             ]);
 
         $this->assertEquals(true, $project->hasPageAssessments());
+    }
+
+    /**
+     * Get the URL to the assessment badge on Commons.
+     */
+    public function testAssessmentBadgeURL()
+    {
+        $projectRepo = $this->getMock(ProjectRepository::class);
+        $projectRepo->expects($this->exactly(2))
+            ->method('getAssessmentsConfig')
+            ->willReturn([
+                'class' => [
+                    'C' => [
+                        'badge' => 'e/e6/Symbol_c_class.svg',
+                    ],
+                    'Unknown' => [
+                        'badge' => 'e/e0/Symbol_question.svg',
+                    ],
+                ],
+            ]);
+        $project = new Project('testWiki');
+        $project->setRepository($projectRepo);
+
+        $this->assertEquals(
+            'https://upload.wikimedia.org/wikipedia/commons/e/e6/Symbol_c_class.svg',
+            $project->getAssessmentBadgeURL('C')
+        );
+
+        // Unknown class.
+        $this->assertEquals(
+            'https://upload.wikimedia.org/wikipedia/commons/e/e0/Symbol_question.svg',
+            $project->getAssessmentBadgeURL('D')
+        );
+    }
+
+    /**
+     * Normalized, quoted table name.
+     */
+    public function testTableName()
+    {
+        $projectRepo = $this->getMock(ProjectRepository::class);
+        $projectRepo->expects($this->once())
+            ->method('getTableName')
+            ->willReturn('testwiki_p.revision_userindex');
+        $project = new Project('testWiki');
+        $project->setRepository($projectRepo);
+        $this->assertEquals(
+            'testwiki_p.revision_userindex',
+            $project->getTableName('testwiki', 'revision')
+        );
     }
 
     /**
