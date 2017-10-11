7a02d4dfbb0aa9209f8636f8738c92b33291175d
Matthew Bowker <matthewrbowker.bugs@gmail.com>
2017-10-11 01:42:35 -0600

    Finishing up this tool

--- src/AppBundle/Controller/DeleteCounterController.php
+++ src/AppBundle/Controller/DeleteCounterController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file contains only the SimpleEditCounterController class.
+ * This file contains only the DeletionCounter class.
  */
 
 namespace AppBundle\Controller;
@@ -23,6 +23,7 @@ class DeleteCounterController extends Controller
 
     /**
      * Get the tool's shortname.
+     *
      * @return string
      */
     public function getToolShortname()
@@ -32,25 +33,34 @@ class DeleteCounterController extends Controller
 
     /**
      * The Simple Edit Counter search form.
-     * @Route("/dc", name="dc")
-     * @Route("/dc", name="DeletionCounter")
-     * @Route("/dc/", name="DeletionCounterSlash")
+     *
+     * @param Request $request The HTTP request.
+     * @param string  $project The project database name or domain.
+     *
+     * @return Response
+     *
+     * @Route("/dc",           name="dc")
+     * @Route("/dc",           name="DeletionCounter")
+     * @Route("/dc/",          name="DeletionCounterSlash")
      * @Route("/dc/index.php", name="DeletionCounterIndexPhp")
      * @Route("/dc/{project}", name="DeletionCounterProject")
-     * @param Request $request The HTTP request.
-     * @param string $project The project database name or domain.
-     * @return Response
      */
     public function indexAction(Request $request, $project = null)
     {
         // Get the query parameters.
         $projectName = $project ?: $request->query->get('project');
-        $username = $request->query->get('username', $request->query->get('user'));
+        $username = $request->query->get(
+            'username',
+            $request->query->get('user')
+        );
 
         // If we've got a project and user, redirect to results.
         if ($projectName != '' && $username != '') {
             $routeParams = [ 'project' => $projectName, 'username' => $username ];
-            return $this->redirectToRoute('DeletionCounterResult', $routeParams);
+            return $this->redirectToRoute(
+                'DeletionCounterResult',
+                $routeParams
+            );
         }
 
         // Instantiate the project if we can, or use the default.
@@ -59,24 +69,34 @@ class DeleteCounterController extends Controller
             : ProjectRepository::getDefaultProject($this->container);
 
         // Show the form.
-        return $this->render('deletionCounter/index.html.twig', [
+        return $this->render(
+            'deletionCounter/index.html.twig',
+            [
             'xtPageTitle' => 'tool-dc',
             'xtSubtitle' => 'tool-dc-desc',
             'xtPage' => 'dc',
             'project' => $theProject,
-        ]);
+            ]
+        );
     }
 
     /**
-     * Display the
-     * @Route("/dc/{project}/{username}", name="DeletionCounterResult")
-     * @param string $project The project domain name.
+     * Display the results
+     *
+     * @param string $project  The project domain name.
      * @param string $username The username.
+     *
      * @return Response
+     *
+     * @Route("/dc/{project}/{username}", name="DeletionCounterResult")
      */
     public function resultAction($project, $username)
     {
-        /** @var Project $project */
+        /**
+         * Project information
+         *
+         * @var Project $project
+         */
         $project = ProjectRepository::getProject($project, $this->container);
         $projectRepo = $project->getRepository();
 
@@ -91,9 +111,17 @@ class DeleteCounterController extends Controller
 
         $dbName = $project->getDatabaseName();
 
-        $loggingTable = $projectRepo->getTableName($dbName, "logging", "userindex");
+        $loggingTable = $projectRepo->getTableName(
+            $dbName,
+            "logging",
+            "userindex"
+        );
 
-        /** @var Connection $conn */
+        /**
+         * Connection to the database
+         *
+         * @var Connection $conn
+         */
         $conn = $this->get('doctrine')->getManager('replicas')->getConnection();
 
         $types = $this->container->getParameter("deletion_counter");
@@ -126,32 +154,45 @@ class DeleteCounterController extends Controller
 
         if ($resultQuery->errorCode() > 0) {
             $this->addFlash('notice', [ 'no-result', $username ]);
-            return $this->redirectToRoute('DeletionCounterProject', [ 'project' => $project->getDomain() ]);
+            return $this->redirectToRoute(
+                'DeletionCounterProject',
+                [
+                    'project' => $project->getDomain()
+                ]
+            );
         }
+
+        $total = 0;
 
         foreach ($resultQuery->fetchAll() as $row) {
             $results[$row["type"]] = $row["count"];
+            $total += $row["count"];
         }
 
-        dump($results);
-
-        // Unknown user - If the user is created the $results variable will have 3 entries.
+        // Unknown user or generated no results
         // This is a workaround to detect non-existent IPs.
         if (count($results) == 0) {
             $this->addFlash('notice', [ 'no-result', $username ]);
 
-            return $this->redirectToRoute('DeletionCounterProject', [ 'project' => $project->getDomain() ]);
+            return $this->redirectToRoute(
+                'DeletionCounterProject',
+                [
+                    'project' => $project->getDomain()
+                ]
+            );
         }
 
-
-
         // Assign the values and display the template
-        return $this->render('deletionCounter/result.html.twig', [
-            'xtPage' => 'dc',
-            'xtTitle' => $username,
-            'user' => $user,
-            'project' => $project,
-            'results' => $results,
-        ]);
+        return $this->render(
+            'deletionCounter/result.html.twig',
+            [
+                'xtPage' => 'dc',
+                'xtTitle' => $username,
+                'user' => $user,
+                'project' => $project,
+                'results' => $results,
+                'total' => $total,
+            ]
+        );
     }
 }
