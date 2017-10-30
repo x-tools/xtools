9352e4ee1773b12fd81ab170a56b42c1eaf4cd04
Matthew Bowker <matthewrbowker.bugs@gmail.com>
2017-10-30 15:09:50 -0600

    Web-based tagging interface

--- src/AppBundle/Controller/MetaController.php
+++ src/AppBundle/Controller/MetaController.php
@@ -7,6 +7,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
 use DateTime;
 use Symfony\Component\HttpFoundation\Response;
@@ -59,8 +60,9 @@ class MetaController extends Controller
             ->getManager($db)
             ->getConnection();
 
-        $query = $client->prepare("SELECT * FROM $table
-                                 WHERE date >= :start AND date <= :end");
+        $query = $client->prepare(
+            "SELECT * FROM $table WHERE date >= :start AND date <= :end"
+        );
         $query->bindParam('start', $start);
         $query->bindParam('end', $end);
         $query->execute();
@@ -77,7 +79,11 @@ class MetaController extends Controller
         $grandSum = 0;
 
         // Generate array of date labels
-        for ($dateObj = new DateTime($start); $dateObj <= $endObj; $dateObj->modify('+1 day')) {
+        for (
+            $dateObj = new DateTime($start);
+            $dateObj <= $endObj;
+            $dateObj->modify('+1 day')
+        ) {
             $dateLabels[] = $dateObj->format('Y-m-d');
         }
 
@@ -99,7 +105,9 @@ class MetaController extends Controller
         }
         arsort($totals);
 
-        return $this->render('meta/result.html.twig', [
+        return $this->render(
+            'meta/result.html.twig',
+            [
             'xtPage' => 'meta',
             'start' => $start,
             'end' => $end,
@@ -108,6 +116,110 @@ class MetaController extends Controller
             'grandSum' => $grandSum,
             'dateLabels' => $dateLabels,
             'timeline' => $timeline,
-        ]);
+            ]
+        );
     }
+
+    /**
+     * Display the tagging interface.
+     *
+     * @Route("/meta/tag", name="MetaTag")
+     *
+     * @return Response
+     */
+    public function tagIndexAction()
+    {
+        if ($this->container->getParameter("kernel.environment") !== "dev") {
+            // We don't want any more than this, this must be hidden entirely on
+            // prod environments.
+            throw new FileNotFoundException();
+        }
+
+        $path = realpath($this->getParameter('kernel.root_dir').'/..');
+
+        $currentVersion = $this->container->getParameter("app.version");
+
+        $request = Request::createFromGlobals();
+
+        $newVersion = $request->get("version");
+        $newReleaseNotes = $request->get("releaseNotes");
+        $newGit = $request->get("git");
+
+        dump($request);
+
+        if ($newVersion !== null) {
+
+            $fRL = "$path/RELEASE_NOTES.md";
+            $fAV = "$path/app/config/version.yml";
+            $fRM = "$path/README.md";
+
+            // Release Notes First
+
+            $releaseNotes = file_get_contents($fRL);
+
+            $releaseNotes = str_replace("# Release Notes #", "", $releaseNotes);
+
+            $releaseNotes = $newReleaseNotes . "\r\n\r\n" . $releaseNotes;
+
+            $releaseNotes = "## $newVersion ##" . "\r\n" . $releaseNotes;
+
+            $releaseNotes = "# Release Notes #\r\n\r\n$releaseNotes";
+
+            file_put_contents($fRL, $releaseNotes);
+
+            unset($releaseNotes);
+
+            // Version parameter next
+
+            $version = file_get_contents($fAV);
+
+            $version = str_replace($currentVersion, $newVersion, $version);
+
+            file_put_contents($fAV, $version);
+
+            unset($version);
+
+            // Readme
+
+            $readme = file_get_contents($fRM);
+
+            $readme = str_replace($currentVersion, $newVersion, $readme);
+
+            file_put_contents($fRM, $readme);
+
+            $this->addFlash("success", "Files have been saved");
+
+            // Lastly, git operations
+
+            if ($newGit == "on") {
+                chdir($path);
+
+                system("git add $fRL");
+                system("git add $fAV");
+                system("git add $fRM");
+
+                system(
+                    "git commit -m \"Tagging version $newVersion (automated)\""
+                );
+
+                system("git tag -a $newVersion -m \"version $newVersion\"");
+
+                $this->addFlash(
+                    "success",
+                    "Git operations succeeded.  Please \"git push\" to finish."
+                );
+            }
+
+            return ($this->redirectToRoute("MetaTag"));
+        }
+
+        return $this->render(
+            "meta/tagIndex.html.twig",
+            [
+                'xtPage' => 'Tagging Interface',
+                'currentVersion' => $currentVersion
+            ]
+        );
+    }
+
 }
