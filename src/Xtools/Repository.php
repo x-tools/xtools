c450d56910cd3f5cd9b251d2c8f65e0e995db863
MusikAnimal <musikanimal@gmail.com>
2018-02-08 00:45:28 -0500

    Repository: add automatic/configurable query killer, default 10 mins

--- src/Xtools/Repository.php
+++ src/Xtools/Repository.php
@@ -14,6 +14,7 @@ use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use GuzzleHttp\Promise\Promise;
 use DateInterval;
@@ -294,6 +295,10 @@ abstract class Repository
         return $value;
     }
 
+    /********************************
+     * DATABASE INTERACTION HELPERS *
+     ********************************/
+
     /**
      * Creates WHERE conditions with date range to be put in query.
      *
@@ -321,48 +326,77 @@ abstract class Repository
 
     /**
      * Execute a query using the projects connection, handling certain Exceptions.
-     * @param  string $sql
-     * @param  array  $params Parameters to bound to the prepared query.
+     * @param string $sql
+     * @param array $params Parameters to bound to the prepared query.
+     * @param int|null $timeout Maximum statement time in seconds. null will use the
+     *   default specified by the app.query_timeout config parameter.
      * @return \Doctrine\DBAL\Driver\Statement
      * @codeCoverageIgnore
      */
-    public function executeProjectsQuery($sql, $params = [])
+    public function executeProjectsQuery($sql, $params = [], $timeout = null)
     {
         try {
+            $this->setQueryTimeout($timeout);
             return $this->getProjectsConnection()->executeQuery($sql, $params);
         } catch (DriverException $e) {
-            return $this->handleDriverError($e);
+            return $this->handleDriverError($e, $timeout);
         }
     }
 
     /**
      * Execute a query using the projects connection, handling certain Exceptions.
-     * @param  QueryBuilder $qb
+     * @param QueryBuilder $qb
+     * @param int $timeout Maximum statement time in seconds. null will use the
+     *   default specified by the app.query_timeout config parameter.
      * @return \Doctrine\DBAL\Driver\Statement
      * @codeCoverageIgnore
      */
-    public function executeQueryBuilder(QueryBuilder $qb)
+    public function executeQueryBuilder(QueryBuilder $qb, $timeout = null)
     {
         try {
+            $this->setQueryTimeout($timeout);
             return $qb->execute();
         } catch (DriverException $e) {
-            return $this->handleDriverError($e);
+            return $this->handleDriverError($e, $timeout);
         }
     }
 
     /**
      * Special handling of some DriverExceptions, otherwise original Exception is thrown.
      * @param DriverException $e
+     * @param int $timeout Timeout value, if applicable. This is passed to the i18n message.
      * @throws ServiceUnavailableHttpException
      * @throws DriverException
      * @codeCoverageIgnore
      */
-    private function handleDriverError(DriverException $e)
+    private function handleDriverError(DriverException $e, $timeout)
     {
+        // If no value was passed for the $timeout, it must be the default.
+        if ($timeout === null) {
+            $timeout = $this->container->getParameter('app.query_timeout');
+        }
+
         if ($e->getErrorCode() === 1226) {
             throw new ServiceUnavailableHttpException(30, 'error-service-overload', null, 503);
+        } elseif ($e->getErrorCode() === 1969) {
+            throw new HttpException(504, 'error-query-timeout', null, [], $timeout);
         } else {
             throw $e;
         }
+    }
+
+    /**
+     * Set the maximum statement time on the MySQL engine.
+     * @param int|null $timeout In seconds. null will use the default
+     *   specified by the app.query_timeout config parameter.
+     * @codeCoverageIgnore
+     */
+    public function setQueryTimeout($timeout = null)
+    {
+        if ($timeout === null) {
+            $timeout = $this->container->getParameter('app.query_timeout');
+        }
+        $sql = "SET max_statement_time = $timeout";
+        $this->getProjectsConnection()->executeQuery($sql);
     }
 }
