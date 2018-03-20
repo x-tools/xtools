57397e03ef85e8f608aaa379f5a393acccc33638
Matthew Bowker <matthewrbowker.bugs@gmail.com>
2018-03-20 13:14:39 -0600

    Change the logic of AdminScore, as in if a user is created less than a year ago, don't give any benefit.  After a year, multiply by before.
    Bug: T190201

--- src/AppBundle/Controller/AdminScoreController.php
+++ src/AppBundle/Controller/AdminScoreController.php
@@ -187,10 +187,9 @@ class AdminScoreController extends XtoolsController
                     $diff = $date->diff($now);
                     $formula = 365 * $diff->format('%y') + 30 * $diff->format('%m') + $diff->format('%d');
                     if ($formula < 365) {
-                        $value = 0;
-                    } else {
-                        $value = $formula - 365;
+                        $multipliers["account-age-mult"] = 0;
                     }
+                    $value = $formula;
                 }
             }
 
