403a93c8b396eb4d2152aee5feb776844709a4d1
MusikAnimal <musikanimal@gmail.com>
2018-10-10 15:23:37 -0400

    Rm include of ClassLoader, which was removed

--- web/app.php
+++ web/app.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
  * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 // $kernel = new AppCache($kernel);
