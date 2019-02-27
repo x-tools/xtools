dc8c677bf6ec181f25d284048059d61251265c8d
MusikAnimal <musikanimal@gmail.com>
2019-02-27 14:51:57 -0500

    Move remaining files in app directory

--- public/app.php
+++ public/app.php
@@ -6,14 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * @var Composer\Autoload\ClassLoader
  */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__.'/../src/autoload.php';
 
 $kernel = new AppKernel('prod', false);
-// $kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying
-// on the configuration parameter.
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
