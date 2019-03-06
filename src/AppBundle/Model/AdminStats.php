a41f567cf77615f7563902a363f92f750116cb5e
MusikAnimal <musikanimal@gmail.com>
2019-03-06 00:12:20 -0500

    AdminStats: cute little icons for user groups, not ambiguous abbrevs
    Rename the 'group' property in AdminStats to 'type', and remove relic
    references to 'admin' when the tool now handles any arbitrary user group
    and set of rights.
    
    Other code cleanup.

--- src/AppBundle/Model/AdminStats.php
+++ src/AppBundle/Model/AdminStats.php
@@ -8,8 +8,7 @@ declare(strict_types = 1);
 namespace AppBundle\Model;
 
 /**
- * AdminStats returns information about users with administrative
- * rights on a given wiki.
+ * AdminStats returns information about users with rights defined in admin_stats.yml.
  */
 class AdminStats extends Model
 {
@@ -17,12 +16,7 @@ class AdminStats extends Model
     /** @var string[][] Keyed by user name, values are arrays containing actions and counts. */
     protected $adminStats;
 
-    /**
-     * Keys are user names, values are their abbreviated user groups.
-     * If abbreviations are turned on, this will instead be a string of the abbreviated
-     * user groups, separated by slashes.
-     * @var string[]|string
-     */
+    /** @var string[] Keys are user names, values are their user groups. */
     protected $usersAndGroups;
 
     /** @var int Number of users in the relevant group who made any actions within the time period. */
@@ -31,8 +25,8 @@ class AdminStats extends Model
     /** @var string[] Usernames of users who are in the relevant user group (sysop for admins, etc.). */
     private $usersInGroup = [];
 
-    /** @var string Group that we're getting stats for (admin, patrollers, stewards, etc.). See admin_stats.yml */
-    private $group;
+    /** @var string Type that we're getting stats for (admin, patrollers, stewards, etc.). See admin_stats.yml */
+    private $type;
 
     /** @var string[] Which actions to show ('block', 'protect', etc.) */
     private $actions;
@@ -55,7 +49,7 @@ class AdminStats extends Model
         $this->project = $project;
         $this->start = $start;
         $this->end = $end;
-        $this->group = $group;
+        $this->type = $group;
         $this->actions = $actions;
     }
 
@@ -63,9 +57,9 @@ class AdminStats extends Model
      * Get the group for this AdminStats.
      * @return string
      */
-    public function getGroup(): string
+    public function getType(): string
     {
-        return $this->group;
+        return $this->type;
     }
 
     /**
@@ -80,18 +74,16 @@ class AdminStats extends Model
             return $relevantUserGroup;
         }
 
-        return $relevantUserGroup = $this->getRepository()->getRelevantUserGroup($this->group);
+        return $relevantUserGroup = $this->getRepository()->getRelevantUserGroup($this->type);
     }
 
     /**
      * Get the array of statistics for each qualifying user. This may be called ahead of self::getStats() so certain
      * class-level properties will be supplied (such as self::numUsers(), which is called in the view before iterating
      * over the master array of statistics).
-     * @param bool $abbreviateGroups If set, the 'user-groups' list will be a string with abbreivated user groups names,
-     *   as opposed to an array of full-named user groups.
      * @return string[]
      */
-    public function prepareStats(bool $abbreviateGroups = true): array
+    public function prepareStats(): array
     {
         if (isset($this->adminStats)) {
             return $this->adminStats;
@@ -101,10 +93,10 @@ class AdminStats extends Model
         $startDb = date('Ymd000000', $this->start);
         $endDb = date('Ymd235959', $this->end);
 
-        $stats = $this->getRepository()->getStats($this->project, $startDb, $endDb, $this->group, $this->actions);
+        $stats = $this->getRepository()->getStats($this->project, $startDb, $endDb, $this->type, $this->actions);
 
         // Group by username.
-        $stats = $this->groupStatsByUsername($stats, $abbreviateGroups);
+        $stats = $this->groupStatsByUsername($stats);
 
         // Resort, as for some reason the SQL isn't doing this properly.
         uasort($stats, function ($a, $b) {
@@ -120,69 +112,40 @@ class AdminStats extends Model
 
     /**
      * Get users of the project that are capable of making the relevant actions,
-     * keyed by user name with abbreviations for the user groups as the values.
-     * @param bool $abbreviate If set, the keys of the result with be a string containing
-     *   abbreviated versions of their user groups, such as 'A' instead of administrator,
-     *   'CU' instead of CheckUser, etc. If $abbreviate is false, the keys of the result
-     *   will be an array of the full-named user groups.
+     * keyed by user name, with the user groups as the values.
      * @return string[][]
      */
-    public function getUsersAndGroups(bool $abbreviate = true): array
+    public function getUsersAndGroups(): array
     {
-        if ($this->usersAndGroups) {
+        if (isset($this->usersAndGroups)) {
             return $this->usersAndGroups;
         }
 
         /**
-         * Each user group that is considered capable of making 'admin actions'.
+         * Each user group that is considered capable of making the relevant actions for $this->group.
          * @var string[]
          */
-        $adminGroups = $this->getRepository()->getUserGroups($this->project, $this->group);
+        $groupUserGroups = $this->getRepository()->getUserGroups($this->project, $this->type);
 
-        /** @var array $usersAndGroups Keys are the usernames, values are their user groups. */
-        $usersAndGroups = $this->project->getUsersInGroups($adminGroups);
+        $this->usersAndGroups = $this->project->getUsersInGroups($groupUserGroups);
 
-        if (false === $abbreviate || 0 === count($usersAndGroups)) {
-            return $usersAndGroups;
-        }
-
-        /**
-         * Keys are the database-stored names, values are the abbreviations.
-         * FIXME: i18n this somehow.
-         * @var string[]
-         */
-        $userGroupAbbrMap = [
-            'sysop' => 'A',
-            'bureaucrat' => 'B',
-            'steward' => 'S',
-            'checkuser' => 'CU',
-            'oversight' => 'OS',
-            'interface-admin' => 'IA',
-            'bot' => 'Bot',
-            'global-renamer' => 'GR',
-        ];
-
-        foreach ($usersAndGroups as $user => $groups) {
-            $abbrGroups = [];
-
-            // Keep track of actual number of sysops.
-            if (in_array($this->getRelevantUserGroup(), $groups)) {
-                $this->usersInGroup[] = $user;
-            }
-
-            foreach ($groups as $group) {
-                if (isset($userGroupAbbrMap[$group])) {
-                    $abbrGroups[] = $userGroupAbbrMap[$group];
-                }
-            }
-
-            // Make 'A' (admin) come before 'CU' (CheckUser), etc.
-            sort($abbrGroups);
-
-            $this->usersAndGroups[$user] = implode('/', $abbrGroups);
-        }
+        // Populate $this->usersInGroup with users who are in the relevant user group for $this->group.
+        $this->usersInGroup = array_keys(array_filter($this->usersAndGroups, function ($groups) {
+            return in_array($this->getRelevantUserGroup(), $groups);
+        }));
 
         return $this->usersAndGroups;
+    }
+
+    public function getUserGroupIcons(): array
+    {
+        // Quick cache, valid only for the same request.
+        static $userGroupIcons = null;
+        if (null !== $userGroupIcons) {
+            return $userGroupIcons;
+        }
+        $userGroupIcons = $this->getRepository()->getUserGroupIcons();
+        return $userGroupIcons;
     }
 
     /**
@@ -196,14 +159,12 @@ class AdminStats extends Model
 
     /**
      * Get the master array of statistics for each qualifying user.
-     * @param bool $abbreviateGroups If set, the 'user-groups' list will be a string with abbreviated user groups names,
-     *   as opposed to an array of full-named user groups.
      * @return string[]
      */
-    public function getStats(bool $abbreviateGroups = true): array
+    public function getStats(): array
     {
         if (isset($this->adminStats)) {
-            $this->adminStats = $this->prepareStats($abbreviateGroups);
+            $this->adminStats = $this->prepareStats();
         }
         return $this->adminStats;
     }
@@ -223,15 +184,13 @@ class AdminStats extends Model
      * Given the data returned by AdminStatsRepository::getStats, return the stats keyed by user name,
      * adding in a key/value for user groups.
      * @param string[][] $data As retrieved by AdminStatsRepository::getStats
-     * @param bool $abbreviateGroups If set, the 'user-groups' list will be a string with abbreviated user groups names,
-     *   as opposed to an array of full-named user groups.
      * @return string[] Stats keyed by user name.
      * Functionality covered in test for self::getStats().
      * @codeCoverageIgnore
      */
-    private function groupStatsByUsername(array $data, bool $abbreviateGroups = true): array
+    private function groupStatsByUsername(array $data): array
     {
-        $usersAndGroups = $this->getUsersAndGroups($abbreviateGroups);
+        $usersAndGroups = $this->getUsersAndGroups();
         $users = [];
 
         foreach ($data as $datum) {
@@ -249,10 +208,10 @@ class AdminStats extends Model
             if (isset($usersAndGroups[$username])) {
                 $users[$username]['user-groups'] = $usersAndGroups[$username];
             } else {
-                $users[$username]['user-groups'] = $abbreviateGroups ? '' : [];
+                $users[$username]['user-groups'] = [];
             }
 
-            // Keep track of non-admins who made admin actions.
+            // Keep track of users who are not in the relevant user group but made applicable actions.
             if (in_array($username, $this->usersInGroup)) {
                 $this->numWithActions++;
             }
