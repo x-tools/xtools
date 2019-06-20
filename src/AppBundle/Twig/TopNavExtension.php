51e55a95b0f6d9cfd9a9425f4876b2c0f1a7291e
MusikAnimal <musikanimal@gmail.com>
2019-06-20 16:37:59 -0400

    Sort top navigation links by localized name
    Ignore testing of Twig function/filter declarations

---
+++ src/AppBundle/Twig/TopNavExtension.php
@@ -0,0 +1,149 @@
+<?php
+declare(strict_types = 1);
+
+namespace AppBundle\Twig;
+
+use Twig\TwigFunction;
+
+/**
+ * Twig functions for top navigation.
+ */
+class TopNavExtension extends AppExtension
+{
+    /** @var string[] Entries for Edit Counter dropdown. */
+    protected $topNavEditCounter;
+
+    /** @var string[] Entries for User dropdown. */
+    protected $topNavUser;
+
+    /** @var string[] Entries for Page dropdown. */
+    protected $topNavPage;
+
+    /** @var string[] Entries for Project dropdown. */
+    protected $topNavProject;
+
+    /**
+     * Twig functions this class provides.
+     * @return TwigFunction[]
+     * @codeCoverageIgnore
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('top_nav_ec', [$this, 'topNavEditCounter']),
+            new TwigFunction('top_nav_user', [$this, 'topNavUser']),
+            new TwigFunction('top_nav_page', [$this, 'topNavPage']),
+            new TwigFunction('top_nav_project', [$this, 'topNavProject']),
+        ];
+    }
+
+    /**
+     * Sorted list of links for the Edit Counter dropdown.
+     * @return string[] Keys are tool IDs, values are the localized labels.
+     */
+    public function topNavEditCounter(): array
+    {
+        if (isset($this->topNavEditCounter)) {
+            return $this->topNavEditCounter;
+        }
+
+        $toolsMessages = [
+            'EditCounterGeneralStatsIndex' => 'general-stats',
+            'EditCounterMonthCountsIndex' => 'month-counts',
+            'EditCounterNamespaceTotalsIndex' => 'namespace-totals',
+            'EditCounterRightsChangesIndex' => 'rights-changes',
+            'EditCounterTimecardIndex' => 'timecard',
+            'TopEdits' => 'top-edited-pages',
+            'EditCounterYearCountsIndex' => 'year-counts',
+        ];
+
+        $this->topNavEditCounter = $this->sortEntries($toolsMessages, 'EditCounter');
+        return $this->topNavEditCounter;
+    }
+
+    /**
+     * Sorted list of links for the User dropdown.
+     * @return string[] Keys are tool IDs, values are the localized labels.
+     */
+    public function topNavUser(): array
+    {
+        if (isset($this->topNavUser)) {
+            return $this->topNavUser;
+        }
+
+        $toolsMessages = [
+            'AdminScore' => 'tool-adminscore',
+            'AutoEdits' => 'tool-autoedits',
+            'CategoryEdits' => 'tool-categoryedits',
+            'EditCounter' => 'tool-editcounter',
+            'EditSummary' => 'tool-editsummary',
+            'GlobalContribs' => 'tool-globalcontribs',
+            'Pages' => 'tool-pages',
+            'EditCounterRightsChangesIndex' => 'rights-changes',
+            'SimpleEditCounter' => 'tool-simpleeditcounter',
+            'TopEdits' => 'tool-topedits',
+        ];
+
+        $this->topNavUser = $this->sortEntries($toolsMessages);
+        return $this->topNavUser;
+    }
+
+    /**
+     * Sorted list of links for the Page dropdown.
+     * @return string[] Keys are tool IDs, values are the localized labels.
+     */
+    public function topNavPage(): array
+    {
+        if (isset($this->topNavPage)) {
+            return $this->topNavPage;
+        }
+
+        $toolsMessages = [
+            'Authorship' => 'tool-authorship',
+            'ArticleInfo' => 'tool-articleinfo',
+        ];
+
+        $this->topNavPage = $this->sortEntries($toolsMessages);
+        return $this->topNavPage;
+    }
+
+    /**
+     * Sorted list of links for the Project dropdown.
+     * @return string[] Keys are tool IDs, values are the localized labels.
+     */
+    public function topNavProject(): array
+    {
+        if (isset($this->topNavProject)) {
+            return $this->topNavProject;
+        }
+
+        $toolsMessages = [
+            'AdminStats' => 'tool-adminstats',
+            'PatrollerStats' => 'tool-patrollerstats',
+            'StewardStats' => 'tool-stewardstats',
+        ];
+
+        $this->topNavProject = $this->sortEntries($toolsMessages, 'AdminStats');
+        return $this->topNavProject;
+    }
+
+    /**
+     * Sort the given entries, localizing the labels.
+     * @param array $entries
+     * @param string|null $toolCheck Only make sure this tool is enabled (not individual tools passed in).
+     * @return array
+     */
+    private function sortEntries(array $entries, ?string $toolCheck = null): array
+    {
+        $toolMessages = [];
+
+        foreach ($entries as $tool => $key) {
+            if ($this->toolEnabled($toolCheck ?? $tool)) {
+                $toolMessages[$tool] = $this->i18n->msg($key);
+            }
+        }
+
+        asort($toolMessages);
+        return $toolMessages;
+    }
+}
