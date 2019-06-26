83dfb1db58c3e732a590d3c4bf60065770f72006
MusikAnimal <musikanimal@gmail.com>
2019-06-26 11:59:18 -0400

    Fix DefaultControllerTest failure

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -45,7 +45,7 @@ class DefaultControllerTest extends ControllerTestAdapter
         );
 
         // Make sure all active tools are listed.
-        static::assertCount(12, $crawler->filter('.tool-list a.btn'));
+        static::assertCount(13, $crawler->filter('.tool-list a.btn'));
     }
 
     /**
