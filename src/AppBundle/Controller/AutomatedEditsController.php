1e7765df41dd3bc7ee713402c0ae9e4464fd513c
MusikAnimal <musikanimal@gmail.com>
2020-12-10 22:38:57 -0500

    AutoEdits: do some validation of on-wiki conifg
    Bug: T262631

--- src/AppBundle/Controller/AutomatedEditsController.php
+++ src/AppBundle/Controller/AutomatedEditsController.php
@@ -109,6 +109,12 @@ class AutomatedEditsController extends XtoolsController
         $autoEditsRepo = new AutoEditsRepository($useSandbox);
         $autoEditsRepo->setContainer($this->container);
 
+        $misconfigured = $autoEditsRepo->getInvalidTools($this->project);
+        foreach ($misconfigured as $tool) {
+            $helpLink = "https://w.wiki/ppr";
+            $this->addFlashMessage('warning', 'auto-edits-misconfiguration', [$tool, $helpLink]);
+        }
+
         // Validate tool.
         // FIXME: instead of redirecting to index page, show result page listing all tools for that project,
         //  clickable to show edits by the user, etc.
