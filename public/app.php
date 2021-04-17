e39c045e888ace5bb90ae314433d47e2186eff3f
MusikAnimal <musikanimal@gmail.com>
2021-04-17 19:44:28 -0400

    app.php: hardcode trusted proxies for VPS
    This is needed so that generated URLs will use HTTPS. This should be
    configurable, but it's not easy to do in this part of the stack because
    we don't have access to the container yet, and XTools doesn't use a
    .env file. We officially no longer support 3rd party installations,
    anyway.
    
    Bug: T244799

--- public/app.php
+++ public/app.php
@@ -11,6 +11,13 @@ $loader = require __DIR__.'/../src/autoload.php';
 $kernel = new AppKernel('prod', false);
 
 $request = Request::createFromGlobals();
+
+// FIXME: should be configurable if we ever return to supporting 3rd party use of XTools
+Request::setTrustedProxies(
+    ['172.16.0.0/21'], // Wikimedia Cloud Services VPS
+    Request::HEADER_X_FORWARDED_ALL
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
