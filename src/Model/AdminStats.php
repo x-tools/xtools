f7d3b5a1ecc3100ab7b5670302365938fe634865
agoujot <andre.goujot@netc.fr>
2025-05-27 21:07:00 +0200

    + totals row

--- src/Model/AdminStats.php
+++ src/Model/AdminStats.php
@@ -230,6 +230,27 @@ class AdminStats extends Model
     }
 
     /**
+     * Get the "totals" row.
+     * @return array containing as keys the counts.
+     */
+    public function getTotalsRow(): array
+    {
+        $totalsRow = [];
+        foreach($this->adminStats as $username => $object) {
+            foreach($object as $key => $value) {
+                if ($key !== 'username' && $key !== 'user-groups') {
+                    if (isset($totalsRow[$key])) {
+                        $totalsRow[$key] += $value;
+                    } else {
+                        $totalsRow[$key] = $value;
+                    }
+                }
+            }
+        }
+        return $totalsRow;
+    }
+
+    /**
      * Get the total number of users in the relevant user group.
      * @return int
      */
