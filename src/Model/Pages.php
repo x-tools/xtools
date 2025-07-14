d8e3eba40894421861776d75bbe83ee77fe66be0
agoujot <andre.goujot@netc.fr>
2025-07-14 20:59:42 +0200

    *grumble* remove space (lint)

--- src/Model/Pages.php
+++ src/Model/Pages.php
@@ -50,7 +50,7 @@ class Pages extends Model
      * @param int|false $offset Unix timestamp. Used for pagination.
      * @param bool $countsOnly Whether to only get the counts
      */
-    public function __construct (
+    public function __construct(
         PagesRepository $repository,
         Project $project,
         User $user,
