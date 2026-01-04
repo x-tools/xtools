a19ca280cfd8f672a3965fada50b5a7b6c03aed6
MusikAnimal <musikanimal@gmail.com>
2026-01-03 19:10:02 -0500

    Add migration files and index.php to PHPCS, fix lint errors
    Exclude CSS

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return static function ( array $context ) {
+	return new Kernel( $context['APP_ENV'], (bool)$context['APP_DEBUG'] );
 };
